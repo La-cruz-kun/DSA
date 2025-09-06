d7077aab6e0865351287713d7c04763176a710c6
La Cruz <crabraham0@gmail.com>
2025-09-06 12:53:20 +0100

    Time: 5 ms (5.18%), Space: 3.7 MB (21.8%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
@@ -1,18 +1,18 @@
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut prefix = vec![1; nums.len()];
-        let mut suffix = vec![1; nums.len()];
-        for i in 1..nums.len() {
+        let n = nums.len();
+        let mut prefix = vec![1; n];
+        let mut suffix = vec![1; n];
+        let mut result = vec![1; n];
+        for i in 1..n {
             prefix[i] = prefix[i - 1] * nums[i - 1];
         }
-        for i in (0..nums.len() - 1).rev() {
+        for i in (0..n - 1).rev() {
             suffix[i] = suffix[i + 1] * nums[i + 1];
         }
-        let result: Vec<_> = suffix
-            .iter()
-            .zip(prefix.iter())
-            .map(|(x, y)| x * y)
-            .collect();
+        for i in 0..n {
+            result[i] = prefix[i] * suffix[i];
+        }
         result
     }
 }
