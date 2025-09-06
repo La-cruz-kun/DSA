a46f7a7f10e3a260df1f6c5eb622bb796467f009
La Cruz <crabraham0@gmail.com>
2025-09-06 13:52:17 +0100

    Time: 0 ms (100%), Space: 3.9 MB (10.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
@@ -3,16 +3,17 @@ impl Solution {
         let n = nums.len();
         let mut prefix = vec![1; n];
         let mut suffix = vec![1; n];
-        let mut result = vec![1; n];
         for i in 1..n {
             prefix[i] = prefix[i - 1] * nums[i - 1];
         }
         for i in (0..n - 1).rev() {
             suffix[i] = suffix[i + 1] * nums[i + 1];
         }
-        for i in 0..n {
-            result[i] = prefix[i] * suffix[i];
-        }
+        let result: Vec<_> = suffix
+            .iter()
+            .zip(prefix.iter())
+            .map(|(x, y)| x * y)
+            .collect();
         result
     }
 }
