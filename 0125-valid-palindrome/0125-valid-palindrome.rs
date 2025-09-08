6f6bb460d91a625e3206b130dc6dfbc068f4a4fa
La Cruz <crabraham0@gmail.com>
2025-09-08 17:53:35 +0100

    Time: 1 ms (25.47%), Space: 2.6 MB (25.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.rs
+++ 0125-valid-palindrome/0125-valid-palindrome.rs
@@ -3,11 +3,6 @@ impl Solution {
         let t: String = s.to_lowercase().matches(char::is_alphanumeric).collect();
         let n = t.len();
         let t: Vec<_> = t.chars().collect();
-        for i in 0..n {
-            if t[i] != t[n - 1 - i] {
-                return false;
-            }
-        }
-        true
+        (0..n / 2).all(|x| t[x] == t[n - 1 - x])
     }
 }
