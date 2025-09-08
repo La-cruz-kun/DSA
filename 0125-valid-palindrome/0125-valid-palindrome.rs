ecad77520cd98b220293484bbac6af401ef0a543
La Cruz <crabraham0@gmail.com>
2025-09-08 18:08:27 +0100

    Time: 2 ms (16.49%), Space: 3.6 MB (7.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.rs
+++ 0125-valid-palindrome/0125-valid-palindrome.rs
@@ -1,8 +1,8 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let t: String = s.to_lowercase().matches(char::is_alphanumeric).collect();
+        let v = s.to_lowercase();
+        let t: Vec<_> = v.matches(char::is_alphanumeric).collect();
         let n = t.len();
-        let t: Vec<_> = t.chars().collect();
         (0..n / 2).all(|x| t[x] == t[n - 1 - x])
     }
 }
